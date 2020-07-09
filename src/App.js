db1a846f2306645c4c01d23201c4291d3557271c
김언동 <kud@KUD.local>
2020-07-09 16:25:23 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  state = {
+    counter: 0
+  };
+  add = () =>{
+    console.log("Add");
+    this.setState(current => ({ counter : current.counter + 1}))
+  };
+  minus = () =>{
+    console.log("Minus");
+    //this.setState({counter:this.state.counter - 1 })
+    this.setState(current => ({ counter : current.counter - 1}))
+  };
+  render(){
+    return(
+    <div>
+      <h1>i am class {this.state.counter} </h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
-  );
+    );
+  }
 }
-
 export default App;
